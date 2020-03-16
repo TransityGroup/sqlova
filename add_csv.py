8b2e7bde465c012eedd9920c2e2df9d861ebceed
Ryan <ryanthomas1998@gmail.com>
2020-03-16 12:01:44 -0400

    actually fix bug

--- add_csv.py
+++ add_csv.py
@@ -87,9 +87,7 @@ def sql_to_json(table_id, sql_path, json_file_name):
         columns.append(column['column_name'])
         type = str(column['data_type'])
         # TODO: add more datatypes
-        print(type)
-        if((type is 'integer') or (type is 'numeric')):
-            print('real')
+        if((type == 'integer') or (type == 'numeric')):
             types.append('real')
         else:
             types.append('text')
